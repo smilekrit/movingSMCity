4793b70ec505b6185741b35e66997a1cb2816f93
smilekrit <smilekrit@gmail.com>
2019-01-20 18:29:29 +0200

    Add files via upload
    charching

--- sketch_jan20b.ino
+++ sketch_jan20b.ino
@@ -14,11 +14,22 @@ void setup() {
 }
 
 void loop() {
+  byteToSend = 0;
   // put your main code here, to run repeatedly:
-    byteToSend = 0; // Обнуляем байт при каждом проходе 
-    bitWrite(byteToSend, , HIGH); // При bitPos=0 получим B00000001, при bitPos=1 - B00000010, при bitPos=2 - B00000100 и т.д. 
+  for(int i = 0;i < 8;i++){
+     // Обнуляем байт при каждом проходе 
+    bitWrite(byteToSend, i , HIGH);// При bitPos=0 получим B00000001, при bitPos=1 - B00000010, при bitPos=2 - B00000100 и т.д. 
+    digitalWrite(latchPin, LOW);  // устанавливаем синхронизацию "защелки" на LOW
+    shiftOut(dataPin, clockPin, MSBFIRST, byteToSend); // Инвертируем сигнал при помощи MSBFIRST, грузим с первого бита 
+    digitalWrite(latchPin, HIGH); //"защелкиваем" регистр, тем самым устанавливая значения на выходах
+    delay(150);
+  } 
+  for(int i = 0;i < 8;i++){
+     // Обнуляем байт при каждом проходе 
+    bitWrite(byteToSend, i , LOW);// При bitPos=0 получим B00000001, при bitPos=1 - B00000010, при bitPos=2 - B00000100 и т.д. 
     digitalWrite(latchPin, LOW);  // устанавливаем синхронизацию "защелки" на LOW
     shiftOut(dataPin, clockPin, LSBFIRST, byteToSend); // Инвертируем сигнал при помощи MSBFIRST, грузим с первого бита 
     digitalWrite(latchPin, HIGH); //"защелкиваем" регистр, тем самым устанавливая значения на выходах
-    delay(150); 
+    delay(150);
+  } 
 }
